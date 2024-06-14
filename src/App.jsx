d12a1ef3c617e9ba2ab73c1a4ec185b3675726d1
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-06-14 15:38:24 +0530

    Created search bar & time and location components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import TimeAndLocation from "./components/TimeAndLocation"
+import TopButtons from "./components/TopButtons"
+import Inputs from "./components/inputs"
 
 
 const App = () => {
   return (
-    <div>
-    <h1 className="text-7xl ">Hlloo</h1>
+    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-tr shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
+      <TopButtons />
+      <Inputs />
+      <TimeAndLocation />
     </div>
   )
 }
