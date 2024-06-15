cf383f707a39a0160494abe730dd52fed3f0bd81
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-06-15 23:52:19 +0530

    updated formattedLocalTime

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,14 +1,19 @@
-const TimeAndLocation = () => {
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const TimeAndLocation = ({ weather }) => {
+    const { formattedLocalTime, name, country } = weather;
     return (
         <div>
             <div className="flex items-center justify-center my-6">
-                <p className="text-2xl font-light tracking-wider ">Tuesday, 13 June 2024 | Local time : 07:21 AM</p>
+                <p className="text-2xl font-light tracking-wider ">{formattedLocalTime}</p>
             </div>
-            <div className="flex items-center justify-center my-3" >
-                <p className="text-3xl font-medium">Berlin, DE</p>
+            <div className="flex items-center justify-center my-3">
+                <p className="text-3xl font-medium">{`${name}, ${country}`}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TimeAndLocation
+export default TimeAndLocation;
