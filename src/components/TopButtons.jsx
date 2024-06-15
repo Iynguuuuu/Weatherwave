8e1b4dc3c87b1fb36dc94bcb600b2cd199b75f4c
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-06-16 01:18:47 +0530

    Added logo

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { TiWeatherPartlySunny } from "react-icons/ti";
 
 const TopButtons = ({ setQuery }) => {
     const cities = [
@@ -27,18 +28,27 @@ const TopButtons = ({ setQuery }) => {
     ];
 
     return (
-        <div className="flex items-center justify-around my-6">
-            {
-                cities.map(city => (
-                    <button
-                        key={city.id}
-                        className="text-2xl font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
-                        onClick={() => setQuery({ q: city.name })}>
-                        {city.name}
-                    </button>
-                ))
-            }
+        <div>
+
+            <h1 className="mx-auto text-[32px] font-extrabold ml-[16rem] tracking-wider flex flex-row ">
+                <span><TiWeatherPartlySunny className="text-[38px] mr-[5px]" /></span>WeatherWave</h1>
+
+
+            <div className="flex items-center justify-around my-6">
+
+                {
+                    cities.map(city => (
+                        <button
+                            key={city.id}
+                            className="text-2xl font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
+                            onClick={() => setQuery({ q: city.name })}>
+                            {city.name}
+                        </button>
+                    ))
+                }
+            </div>
         </div>
+
     );
 };
 
