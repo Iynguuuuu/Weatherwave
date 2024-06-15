9ee8d7ff7e75bba75910b95421347fa013e7ad74
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-06-15 23:51:24 +0530

    Added onclick funtions over cities

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,16 +1,20 @@
-const TopButtons = () => {
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const TopButtons = ({ setQuery }) => {
     const cities = [
         {
             id: 1,
-            name: "London",
+            name: "Colombo",
         },
         {
             id: 2,
-            name: "Sydney",
+            name: "New York",
         },
         {
             id: 3,
-            name: "Tokyo",
+            name: "London",
         },
         {
             id: 4,
@@ -26,9 +30,10 @@ const TopButtons = () => {
         <div className="flex items-center justify-around my-6">
             {
                 cities.map(city => (
-                    <button 
-                        key={city.id} 
-                        className="text-2xl font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in">
+                    <button
+                        key={city.id}
+                        className="text-2xl font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
+                        onClick={() => setQuery({ q: city.name })}>
                         {city.name}
                     </button>
                 ))
