e35e1207e84f1578915ec0f185da3e3dab9afdb6
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-06-16 01:21:18 +0530

    fixed title error in the both forecast section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,53 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { useEffect, useState } from "react"
 import Forecast from "./components/Forecast"
 import TempAndDetails from "./components/TempAndDetails"
 import TimeAndLocation from "./components/TimeAndLocation"
 import TopButtons from "./components/TopButtons"
 import Inputs from "./components/inputs"
+import getFormattedWeatherData from "./services/WeatherServices"
 
 
 const App = () => {
-  return (
-    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-tr shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
-      <TopButtons />
-      <Inputs />
-      <TimeAndLocation />
-      <TempAndDetails/>
-      <Forecast/>
 
+  const [query, setQuery] = useState({ q: "colombo" })
+  const [units, setUnits] = useState("metric")
+  const [weather, setWeather] = useState(null)
+
+  const getWeather = async () => {
+    await getFormattedWeatherData({ ...query, units }).then(data => {
+      setWeather(data);
+    });
+
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [query, units]);
+
+  const formatBackGround = () => {
+    if (!weather) return "from-cyan-600 to-blue-700";
+    const threshold = units === 'metric' ? 20 : 60
+    if (weather.temp <= threshold) "from-cyan-600 to-blue-700";
+    return "from-yellow-600 to-orange-700";
+  }
+
+  return (
+    <div
+      className={`mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-tr shadow-xl shadow-gray-400 ${formatBackGround()}`}>
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} setUnits={setUnits} />
+
+      {weather && (
+        <>
+          <TimeAndLocation weather={weather} />
+          <TempAndDetails weather={weather} />
+          <Forecast title="3 hour forecast" data={weather.hourly} />
+          <Forecast title="daily forecast" data={weather.daily} />
+        </>
+      )}
     </div>
   )
 }
--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,23 +1,25 @@
-const Forecast = () => {
-    const data = [1,2,3,4,5];
-  return (
-    <div>
-        <div className="flex items-center justify-start mt-6">
-            <p className="font-medium uppercase ">3 hour step forecast</p>
-        </div>
-        <hr className="my-1"/>
-        <div className="flex items-center  justify-between">
-            {
-                data.map((data, index)=> (
-                    <div key={index} className="flex flex-col items-center justify-center">
-                        <p>Wed</p>
-                        <img src="http://openweathermap.org/img/wn/01d@2x.png" alt=""/>
-                    </div>
-                ))
-            }
-        </div>
-    </div>
-  )
-}
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 
-export default Forecast
+const Forecast = ({ title, data }) => {
+    return (
+        <div>
+            <div className="flex items-center justify-start mt-6">
+                <p className="font-bold tracking-widest uppercase">{title}</p>
+            </div>
+            <hr className="my-1" />
+            <div className="flex items-center justify-between">
+                {data.map((d, index) => (
+                    <div key={index} className="flex flex-col items-center justify-center">
+                        <p className="text-[18px]">{d.title}</p>
+                        <img className="w-10 my-2" src={d.icon} alt="weather icon" />
+                        <p className="text-[20px]">{`${d.temp.toFixed()}°`}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Forecast;
